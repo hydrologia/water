d2438481bcd4fc914d53efffc7c5e8170fb8763a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-03 22:36:34 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -12,15 +12,21 @@ def main():
 
     logger.info('water')
 
+    src.references.determinands.Determinands().exc()
+
 
 if __name__ == '__main__':
     root = os.getcwd()
     sys.path.append(root)
+    sys.path.append(os.path.join(root, "src"))
 
     # Logging
     logging.basicConfig(level=logging.INFO,
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
     logger = logging.getLogger(__name__)
+
+    # Classes
+    import src.references.determinands
 
     main()
