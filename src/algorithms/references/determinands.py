8025f9622fd8e7882b4e815acc4feef4620b6702
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-06 01:36:44 +0100

    Update src/algorithms/references/determinands.py

--- src/algorithms/references/determinands.py
+++ src/algorithms/references/determinands.py
@@ -21,8 +21,14 @@ class Determinands:
         """
 
         configurations = config.Config()
-        self.query = configurations.reference_query
 
+        self.__focus = 'determinands'
+
+        # self.__query hosts the query string for retrieving determinands data via the environment
+        # agency's API (application programming interface)
+        self.__query = configurations.reference_query
+
+        # The reference's default field names, and alternative names
         self.__fields = {'notation': 'determinand_id', 'label': 'determinand_desc', 'definition': 'definition',
                          'unit.label': 'unit_of_measure', 'unit.comment': 'unit_of_measure_desc'}
 
@@ -32,7 +38,9 @@ class Determinands:
         :return:
         """
 
-        frame: pd.DataFrame = src.interface.measures.Measures().exc(branch=self.query.determinands)
+        print(self.__query.__getattribute__(self.__focus))
+
+        frame: pd.DataFrame = src.interface.measures.Measures().exc(branch=self.__query.determinands)
         frame: pd.DataFrame = frame.copy()[self.__fields.keys()]
         frame.rename(columns=self.__fields, inplace=True)
         frame.info()
