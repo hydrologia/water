6ba367dd9ddf62b1e47bde024a53c46b69bf8042
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-06 02:27:55 +0100

    Add src/algorithms/references/sampling_point.py

---
+++ src/algorithms/references/sampling_point.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+class SamplingPoint:
+
+    def __init__(self):
+        """
+
+        """
+
+    def exc(self, subarea: pd.DataFrame, sampling_point_types: pd.DataFrame):
+        """
+
+        :return:
+        """
