5b57febcd184f8468a65fe674938930f09a312a6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-06 02:25:41 +0100

    Update config.py

--- config.py
+++ config.py
@@ -20,19 +20,17 @@ class Config:
         self.endpoint = 'http://environment.data.gov.uk/water-quality/{branch}'
 
         # The reference query
-        __ReferenceQuery = collections.namedtuple(typename='ReferenceQuery',
-                                                  field_names=['determinands', 'sampling_point_types',
-                                                               'environment_agency_area', 'environment_agency_subarea',
-                                                               'sampling_point'])
+        __ReferenceQuery = collections.namedtuple(
+            typename='ReferenceQuery', field_names=['determinands', 'sampling_point_types', 'environment_agency_area',
+                                                    'environment_agency_subarea', 'sampling_point'])
+        self.reference_query = __ReferenceQuery(
+            determinands='def/determinands.csv', sampling_point_types='sampling-point-types.csv',
+            environment_agency_area='ea-area.csv', environment_agency_subarea='ea-subarea.csv',
+            sampling_point='sampling-point.csv')
 
-        self.reference_query = __ReferenceQuery(determinands='def/determinands.csv',
-                                                sampling_point_types='sampling-point-types.csv',
-                                                environment_agency_area='ea-area.csv',
-                                                environment_agency_subarea='ea-subarea.csv',
-                                                sampling_point='sampling-point.csv')
-
-        # The reference data directories
-        __ReferenceDirectory = collections.namedtuple(typename='ReferenceDirectory', field_names=['raw', 'structured'])
-
-        self.reference_directory = __ReferenceDirectory(raw=os.path.join(os.getcwd(), 'data', 'references'),
-                                                        structured=os.path.join(os.getcwd(), 'warehouse', 'references'))
+        # The reference data directories.
+        __ReferenceDirectory = collections.namedtuple(
+            typename='ReferenceDirectory', field_names=['raw', 'structured'])
+        self.reference_directory = __ReferenceDirectory(
+            raw=os.path.join(os.getcwd(), 'data', 'references'),
+            structured=os.path.join(os.getcwd(), 'warehouse', 'references'))
