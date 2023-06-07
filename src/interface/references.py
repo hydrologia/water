9b9f5379af17e8142a64f86f5e78e71f49031ddb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-07 22:16:17 +0100

    Add src/interface/references.py

---
+++ src/interface/references.py
@@ -0,0 +1,54 @@
+import yaml
+import os
+
+import config
+
+
+class References:
+
+    """
+    Class Connector
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+        # The expected location of the YAML
+        self.__path = os.path.join(os.getcwd(), 'resources', 'references.yaml')
+
+        # Get the stream of database credentials as soon as this class is instantiated
+        self.__stream = self.__get_stream()
+
+    def __get_stream(self) -> dict:
+        """
+        Reads the YAML file of database objects. Each object being the set of connection parameters
+        of a database.
+
+        :return:
+        """
+
+        with open(file=self.__path, mode='r') as stream:
+            try:
+                return yaml.load(stream=stream, Loader=yaml.CLoader)
+            except yaml.YAMLError as err:
+                raise Exception(err)
+
+    @staticmethod
+    def __excerpt(stream: dict, code: str) -> config.Config().Reference_:
+        """
+        Extracts the details of an API (Application Programming Interface) object.
+
+        :param stream: A stream of objects wherein each object has the API details for a data set
+        :param code: The code of the API object of interest.
+        :return:
+        """
+
+        # the dictionary of keys in focus
+        dictionary: dict = [item for item in stream['parameters'] if item['code'] == code][0]
+
+        # the named tuple form of the keys
+        excerpt = config.Config().Reference_(**dictionary)
+
+        return excerpt
