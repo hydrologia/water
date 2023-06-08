3575ffc488f52f135f7496fe03830cc8d3224039
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-08 20:25:57 +0100

    Update src/algorithms/references/subarea.py

--- src/algorithms/references/subarea.py
+++ src/algorithms/references/subarea.py
@@ -1,3 +1,12 @@
+import os
+
+import pandas as pd
+
+import config
+import src.functions.streams
+import src.interface.measures
+import src.interface.references
+
 
 class Subarea:
 
@@ -6,8 +15,54 @@ class Subarea:
 
         """
 
+        # The reference's default field names, and alternative names
+        self.__fields = {'notation': 'subarea_id', 'label': 'subarea_desc',
+                         'subOrganizationOf': 'area_id'}
+
+        # The API parameters of the determinands reference data
+        self.__references = src.interface.references.References().exc(code="subarea")
+
+        # Writing
+        self.__streams = src.functions.streams.Streams()
+
+        # self.__directory hosts the directory names for raw & structured reference data.
+        configurations = config.Config()
+        self.__directory = configurations.references()
+
+    def __write(self, blob: pd.DataFrame, root: str):
+        """
+
+        :param blob:
+        :param root:
+        :return:
+        """
+
+        self.__streams.write(data=blob, path=os.path.join(root, f'{self.__references.basename}'))
+
+    def __structure(self, blob: pd.DataFrame):
+        """
+
+        :param blob:
+        :return:
+        """
+
+        # structured
+        frame: pd.DataFrame = blob.copy()[self.__fields.keys()]
+        frame.rename(columns=self.__fields, inplace=True)
+
+        self.__write(blob=frame, root=self.__directory.structured)
+
     def exc(self):
         """
 
         :return:
         """
+
+        frame: pd.DataFrame = src.interface.measures.Measures().exc(
+            query=self.__references.query)
+
+        # Keep a copy of the raw data
+        self.__write(blob=frame, root=self.__directory.raw)
+
+        # Structure and save
+        self.__structure(blob=frame)
