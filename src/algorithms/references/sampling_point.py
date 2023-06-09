e539a401d557e31ab3158073c7439d57fc07d1bd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-09 01:02:43 +0100

    Update src/algorithms/references/sampling_point.py

--- src/algorithms/references/sampling_point.py
+++ src/algorithms/references/sampling_point.py
@@ -74,7 +74,7 @@ class SamplingPoint:
         """
 
         frame: pd.DataFrame = src.interface.measures.Measures().exc(
-            query=self.__references.query)
+            affix=self.__references.affix)
 
         # Keep a copy of the raw data
         self.__write(blob=frame, root=self.__directory.raw)
