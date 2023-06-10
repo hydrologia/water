3f2e531b485ed7344db194e1575f9c0a4a8f2fa2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-10 22:05:27 +0100

    Update src/algorithms/interface.py

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -12,45 +12,77 @@ import src.algorithms.measurements
 class Interface:
 
     def __init__(self):
+        """
 
-        configurations = config.Config()
+        """
+
+        self.__years = config.Config().years
 
         # Streams
         self.__streams = src.functions.streams.Streams()
 
         # The water integrity data, and hydrological data, warehouse paths
-        self.__integrity = os.path.join(os.getcwd(), 'warehouse', 'integrity', '{year}')
+        self.__segments = ['integrity']
+        self.__pathstr = os.path.join(os.getcwd(), 'warehouse', '{segment}', '{year}')
 
-        # Prepare directories
-        self.__years = configurations.years
-        self.__directories = src.functions.directories.Directories()
+    def __directories(self):
+        """
+        Prepare directories
+
+        :return:
+        """
+
+        directories = src.functions.directories.Directories()
+
+        for segment in self.__segments:
+
+            cleanup = [dask.delayed(directories.cleanup)(self.__pathstr.format(segment=segment, year=year))
+                       for year in self.__years]
+            create = [dask.delayed(directories.create)(self.__pathstr.format(segment=segment, year=year))
+                      for year in self.__years]
+
+            dask.compute(cleanup, scheduler='threads')
+            dask.compute(create, scheduler='threads')
 
     def __areas(self) -> np.ndarray:
+        """
+
+        :return:
+        """
 
         filepath = os.path.join(os.getcwd(), 'warehouse', 'references', 'environment_agency_area.csv')
         return self.__streams.read(url=filepath, usecols=['area_id']).array
 
     def __sampled_material_types(self) -> pd.DataFrame:
+        """
+
+        :return:
+        """
 
         filepath = os.path.join(os.getcwd(), 'warehouse', 'references', 'sampled_material_types.csv')
         return self.__streams.read(url=filepath,
                                    usecols=['sampled_material_type_id', 'sampled_material_type_desc'])
 
     def __purposes(self) -> pd.DataFrame:
+        """
+
+        :return:
+        """
 
         filepath = os.path.join(os.getcwd(), 'warehouse', 'references', 'purposes.csv')
         return self.__streams.read(url=filepath,
                                    usecols=['purpose_id', 'purpose_desc'])
 
     def exc(self):
+        """
 
-        [dask.delayed(self.__directories.cleanup)(self.__integrity.format(year=year)) for year in self.__years]
-        [dask.delayed(self.__directories.create)(self.__integrity.format(year=year)) for year in self.__years]
+        :return:
+        """
 
-        # src.algorithms.measurements.Measurements().exc()
+        # Prepare directories
+        self.__directories()
 
-
-
-
-
-
+        # Water integrity measures
+        src.algorithms.measurements.Measurements(
+            sampled_material_types=self.__sampled_material_types(), purposes=self.__purposes())\
+            .exc(years=self.__years, areas=self.__areas())
