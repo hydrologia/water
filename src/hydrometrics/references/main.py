e81bbb10a1656649105d90888c155ce85a7cb71a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-12 21:08:03 +0100

    Add src/hydrometrics/references/main.py

---
+++ src/hydrometrics/references/main.py
@@ -0,0 +1,38 @@
+
+"""
+main.py
+"""
+import logging
+import os
+import sys
+
+
+def main():
+    """
+    The entry point.
+
+    :return:
+    """
+
+    logger.info('hydrology references')
+
+    src.hydrometrics.references.stations.Stations().exc()
+
+
+if __name__ == '__main__':
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.insert(0, os.path.join(root, 'src'))
+
+    # Threads
+    os.environ['NUMEXPR_MAX_THREADS'] = '8'
+
+    # Logging
+    logging.basicConfig(level=logging.INFO, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger(__name__)
+
+    # Classes
+    import src.hydrometrics.references.stations
+
+    main()
