5635dd0e4dc13d912001b56be9c67f5e302606c1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-12 19:44:39 +0100

    Update src/elements/references/interface.py

--- src/elements/references/interface.py
+++ src/elements/references/interface.py
@@ -4,13 +4,13 @@ interface.py
 import pandas as pd
 
 import config
-import src.algorithms.references.area
-import src.algorithms.references.determinands
-import src.algorithms.references.purposes
-import src.algorithms.references.sampled_material_types
-import src.algorithms.references.sampling_point
-import src.algorithms.references.sampling_point_types
-import src.algorithms.references.subarea
+import src.elements.references.area
+import src.elements.references.determinands
+import src.elements.references.purposes
+import src.elements.references.sampled_material_types
+import src.elements.references.sampling_point
+import src.elements.references.sampling_point_types
+import src.elements.references.subarea
 import src.functions.directories
 
 
@@ -42,12 +42,12 @@ class Interface:
         :return:
         """
 
-        src.algorithms.references.area.Area().exc()
-        src.algorithms.references.determinands.Determinands().exc()
-        src.algorithms.references.purposes.Purposes().exc()
-        src.algorithms.references.sampled_material_types.SamplingMaterialTypes().exc()
-        sampling_point_types: pd.DataFrame = src.algorithms.references.sampling_point_types.SamplingPointTypes().exc()
-        subarea: pd.DataFrame = src.algorithms.references.subarea.Subarea().exc()
+        src.elements.references.area.Area().exc()
+        src.elements.references.determinands.Determinands().exc()
+        src.elements.references.purposes.Purposes().exc()
+        src.elements.references.sampled_material_types.SamplingMaterialTypes().exc()
+        sampling_point_types: pd.DataFrame = src.elements.references.sampling_point_types.SamplingPointTypes().exc()
+        subarea: pd.DataFrame = src.elements.references.subarea.Subarea().exc()
 
-        return src.algorithms.references.sampling_point.SamplingPoint().exc(
+        return src.elements.references.sampling_point.SamplingPoint().exc(
             subarea=subarea, sampling_point_types=sampling_point_types)
